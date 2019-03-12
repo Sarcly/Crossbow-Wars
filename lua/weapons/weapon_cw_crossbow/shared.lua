bdfc8c1887fe44668ff59fba21b4abb4a65fa8fa
Myles Horn <myles@MilkyWay.com>
2019-03-11 21:05:07 -0500

    cleanup

--- lua/weapons/weapon_cw_crossbow/shared.lua
+++ lua/weapons/weapon_cw_crossbow/shared.lua
@@ -50,9 +50,6 @@ function SWEP:Initialize()
 end
 
 function SWEP:PrimaryAttack()
-    print("===")
-    print(self:Ammo1())
-    print(self:Clip1())
     if !(self.Weapon:GetNextPrimaryFire() < CurTime()) then return end
     if (self:GetNetworkedBool("cwreloading")) then return end
     if (self:Ammo1()<=0 and self:Clip1()<=0) then return end
